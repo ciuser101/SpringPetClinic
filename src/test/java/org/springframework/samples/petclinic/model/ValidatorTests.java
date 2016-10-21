ee99ff5053412a27a78b94e31e6c9cf25d0e9b57
Sriramulu Parisa <sriramulu.parisa@kony.com>
2016-10-21 15:19:36 +0530

    Warning & Test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -109,6 +109,23 @@ public class ValidatorTests {
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
         assertThat(violation.getMessage()).isEqualTo("may not be empty");
 	}
+    
+    
+    @Test
+   	public void testShouldNotValidateWhenFirstNameEmpty22() {
+       	LocaleContextHolder.setLocale(Locale.ENGLISH);
+           Person person = new Person();
+           person.setFirstName("");
+           person.setLastName("smith");
+
+           Validator validator = createValidator();
+           Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
+
+           assertThat(constraintViolations.size()).isEqualTo(1);
+           ConstraintViolation<Person> violation = constraintViolations.iterator().next();
+           assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
+           assertThat(violation.getMessage()).isEqualTo("may not be empty");
+   	}
 
 
 }
