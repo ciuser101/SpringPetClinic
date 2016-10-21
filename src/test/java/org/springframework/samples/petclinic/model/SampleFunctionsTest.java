296156c47ae9fdf5978dc69c08d33890aab4d1c5
Sriramulu Parisa <sriramulu.parisa@kony.com>
2016-10-21 13:22:05 +0530

    Test

---
+++ src/test/java/org/springframework/samples/petclinic/model/SampleFunctionsTest.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class SampleFunctionsTest {
+
+	@Test
+	public void testAdditionOfNumbers() {
+        String oldName = "AA";
+        String newName = "A" + "A";
+        assertThat(oldName).isEqualTo(newName);
+	}
+}
