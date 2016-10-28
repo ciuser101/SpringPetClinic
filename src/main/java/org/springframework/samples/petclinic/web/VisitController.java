dae3452b78fa2694a65c13b584df10459055fa5a
Sriramulu Parisa <sriramulu.parisa@kony.com>
2016-10-28 14:55:47 +0530

    Test Added

--- src/main/java/org/springframework/samples/petclinic/web/VisitController.java
+++ src/main/java/org/springframework/samples/petclinic/web/VisitController.java
@@ -32,6 +32,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Juergen Hoeller
  * @author Ken Krebs
--- src/test/java/org/springframework/samples/petclinic/web/VisitControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/VisitControllerTests.java
@@ -1,5 +1,15 @@
 package org.springframework.samples.petclinic.web;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,11 +21,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Test class for {@link VisitController}
@@ -87,6 +92,14 @@ public class VisitControllerTests {
             .andExpect(view().name("visitList"));
     }
 
+    
+    @Test
+    public void testShowVisits2() throws Exception {
+        mockMvc.perform(get("/owners/*/pets/{petId}/visits", TEST_PET_ID))
+            .andExpect(status().isOk())
+            .andExpect(model().attributeExists("visits"))
+            .andExpect(view().name("visitList"));
+    }
 
 
 }
